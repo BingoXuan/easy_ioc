38a2fec8532f2e9097805b9bd38653af3f062dec
zebing-xuan <zebing.xuan@prmeasure.com>
2022-08-20 20:28:30 +0800

    branch for python3 to python3.4

--- easy_ioc/inject.py
+++ easy_ioc/inject.py
@@ -1,28 +1,26 @@
-import typing
 from easy_ioc.injectable import Injectable
 
-T = typing.TypeVar('T')
 
-
-def inject(cls: T) -> T:
+def inject(cls):
     """
-    if you're using python3.7 or higher
-    inject is typed, it returns a descriptor with your input
-    inject accepts Type[T]
-    it will hint T or Type[T]
-    when you assess as an attribute of an object
-    the descriptor always returns Type[T] or T
-    the type is safe
-    when no dependency has been injected
-    it raises an exception
-    >>> class ToBeInjected:
-    >>>     pass
+    if you're using python3.5 or lower,
+    function inject may hint nothing to return.
+    Don't worry. we have a solution to fix it.
+    >>> class ToBeInject:
+    >>>     def do_something(self):
+    >>>         pass
     >>> class A:
-    >>>     b = inject(ToBeInjected)
-    >>>     # ide like pycharm or editor plugin like jedi
-    >>>     # will tell you type of b is ToBeInjected
-    :param cls: T as a generic Type
-    :return: T
+    >>>     b = inject(ToBeInject) # type: ToBeInject
+    >>>     def __init__(self):
+    >>>         # for tell ide or editor type of self.b
+    >>>         # use assert or typed be comment
+    >>>         assert isinstance(self.b,ToBeInject)
+    >>>         # if dependencies injected properly,
+    >>>         # b is instance of ToBeInject
+    >>>         # otherwise, it raises Exception
+    >>>         # because you injected in wrong way
+    :param cls: any
+    :return: any
     """
     return Injectable(cls)
 
