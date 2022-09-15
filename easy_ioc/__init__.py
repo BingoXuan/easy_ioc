4e2164d7fa95dd44c5b0944a34962df53139c98f
zebing-xuan <xuanzebing.bingo@gmail.com>
2022-09-15 12:21:46 +0800

    0.1.4

--- easy_ioc/__init__.py
+++ easy_ioc/__init__.py
@@ -1,5 +1,5 @@
 from easy_ioc.inject import inject
 from easy_ioc.ioc import Container
 from easy_ioc.injectable import DependencyError
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __all__ = ['Container', 'DependencyError', 'inject']
